178a5d3d0d77a0bfa749dd8429d9ba6cd41d544d
Oneil Williams <oneilswilliams@gmail.com>
2019-04-25 21:00:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) { 
   if (string.toLowerCase() == string) {return "I can't hear you!"}
   if (string.toUpperCase()== string) {return "YES INDEED!"}
-  if (s)
+  if (string == "I love you, Grandma.") {return "I love you, too."}
 }
     
  

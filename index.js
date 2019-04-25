c0f5e05bbdebcf80a5dde671574a585ae1445836
Oneil Williams <oneilswilliams@gmail.com>
2019-04-25 19:28:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log('HELLO'); 'hello'.toUpperCase()
+}
+function logWhisper(string) {
+  console.log('hello'); 'HELLO'.toLowerCase()
+}
+
+var uppercase = "HELLO!";
+uppercase.toUpperCase();
+var lowercase = "hello!";
+lowercase.toLowerCase();
+
+function sayHiToGrandma(string) { 
+  {
+ console.log ("I can't hear you!"); "hello".lowercase();}
+  {
+  console.log("YES INDEED!"); "HELLO".uppercase();}
+  {
+  console.log ("I love you, too."); "I love you, Grandma. "}
+}
